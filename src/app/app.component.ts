ed772af872602fb776e2985e6206efb21ece1c67
Reda <mahmoudreda457@gmail.com>
2026-05-22 17:46:56 +0300

    Split support tickets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './server-status/server-status.component';
 import { TrafficComponent } from './traffic/traffic.component';
+import { SupportTicketsComponent } from './support-tickets/support-tickets.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent],
+  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, SupportTicketsComponent],
 })
 export class AppComponent {}
