b733674c50cc49b5189e76327e16ee914d64d9ae
Reda <mahmoudreda457@gmail.com>
2026-05-23 10:40:40 +0300

    Tickets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
 import { TrafficComponent } from './dashboard/traffic/traffic.component';
-import { SupportTicketsComponent } from './dashboard/support-tickets/support-tickets.component';
 import { DashbpardItemComponent } from './dashboard/dashbpard-item/dashbpard-item.component';
+import { TicketsComponent } from './dashboard/tickets/tickets.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, SupportTicketsComponent, DashbpardItemComponent],
+  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent, DashbpardItemComponent],
 })
 export class AppComponent {}
