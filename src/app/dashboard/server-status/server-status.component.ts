aa9b37c039c32fc99342d8199be03e20f7b682ed
Reda <mahmoudreda457@gmail.com>
2026-05-23 15:33:48 +0300

    Enhance server status component with dynamic class binding and implement status update logic using interval

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-server-status',
   standalone: true,
   imports: [],
   templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+  styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  // private interval?: ReturnType<typeof setInterval>;
+  private destroyRef = inject(DestroyRef)
+
+  constructor() {}
+
+  ngOnInit() {
+    console.log('ON INIT');
+
+    const interval = setInterval(() => {
+      const rnd = Math.random(); // 0 - 0.99999999999
+
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval)
+    })
+  }
+
+  // ngOnDestroy(): void {
+  //   clearTimeout(this.interval)
+  // }
 }
